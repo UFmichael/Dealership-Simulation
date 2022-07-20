f2d562d26afde5ee5a6ab3254f586edb8bc9844c
UFmichael <109604650+UFmichael@users.noreply.github.com>
2022-07-20 00:35:03 -0400

    Update showroom.py

--- showroom.py
+++ showroom.py
@@ -1,1 +1,8 @@
-class Showroom:
+# a class to manage the showroom commands and corresponding information
+#Last Modified: 7/19/2022
+class Showroom():
+  def __init__(self, space):
+    self.space = space
+
+  def add_car():
+    num_car_inside += 1
