79a2528f977248b4f9fe752767b44e120039428c
UFmichael <109604650+UFmichael@users.noreply.github.com>
2022-07-20 12:23:40 -0400

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,5 +1,5 @@
 # a class to manage the vehicle commands and house the vehicle information
-#Last Modified: 7/19/2022
+#Last Modified: 7/20/2022
 class Vehicle():
   def __init__(self, brand, model, year, price, mileage):
     self.brand = brand
@@ -8,3 +8,9 @@ class Vehicle():
     self.price = price
     self.mileage = mileage
 
+  def display(self): 
+    print("The brand is a "+str(self.brand))
+    print("The model is "+str(self.model))
+    print("The year is "+str(self.year))
+    print("The price is $"+str(self.price))
+    print("The mileage is "+str(self.mileage)+" miles")
