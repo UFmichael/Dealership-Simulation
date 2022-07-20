ba70d0b52022fa8a050a9bc73a089d7cabcc15b0
UFmichael <109604650+UFmichael@users.noreply.github.com>
2022-07-20 00:34:04 -0400

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -1,4 +1,8 @@
+# a class to manage the dealership commands and corresponding information
+#Last Modified: 7/19/2022
 class Dealership():
-  def rooms_full:
-    return True
+  def __init__(self, space):
+    self.space = space
 
+  def add_room():
+    rooms_used += 1
